8a2838ee2d9ab03f5a8511a43f666b76c19cb4ed
passanthamdy <passant.hamdy99@gmail.com>
2022-06-13 19:47:17 +0200

    modify project structure

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import User
 # Register your models here.
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ['first_name', 'last_name', 'id']
+#     class Meta:
+#         model = User
+
+# admin.site.register(User, UserAdmin)
+
 admin.site.register(User)
